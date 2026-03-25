fa0c99025306c5177717a7f4131a20670e30da89
TalhaSelimhan <talhaselimhan@icloud.com>
2026-03-25 21:42:14 +0300

    initial project

---
+++ clipper-zero/clipper-zero/clipper_zeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipper_zeroApp.swift
+//  clipper-zero
+//
+//  Created by Talha on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct clipper_zeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
